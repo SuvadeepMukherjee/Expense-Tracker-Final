fa1e259022fb1be827f50d73c458c30f648aca1c
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-08 13:59:17 +0530

    Made some changes in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -15,16 +14,22 @@ function generateAccessToken(id, email) {
   return jwt.sign({ userId: id, email: email }, process.env.TOKEN);
 }
 
+/*
+  Express route handler: Renders the sign-up page.
+*/
 exports.getSignUpPage = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "sign-up.html"));
 };
 
+/*
+  Express route handler: Renders the login page.
+*/
 exports.getLoginPage = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "login.html"));
 };
 
 /*
-  This function handles user sign-up requests.
+  Express route handler: Handles user sign-up requests.
   - Extracts user information (name, email, and password) from the request body.
   - Checks if the provided email already exists in the database.
     - If the email exists, returns a 409 status with an error message.
@@ -40,34 +45,41 @@ exports.postUserSignUp = async (req, res, next) => {
     const email = req.body.emailValue;
     const password = req.body.passwordValue;
 
-    await User.findOne({ where: { email: email } })
-      .then((user) => {
-        if (user) {
-          res.status(409).json({
-            error: "This email is already taken .Please choose another one",
-          });
-        } else {
-          bcrypt.hash(password, 10, async (err, hash) => {
-            await User.create({
-              name: name,
-              email: email,
-              password: hash,
-            });
-          });
-          res.status(200).json({
-            success: true,
-            message: "Login Successful!",
-          });
-        }
-      })
-      .catch((err) => console.log(err));
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({ where: { email: email } });
+
+    if (existingUser) {
+      // Email already taken
+      return res.status(409).json({
+        error: "This email is already taken. Please choose another one.",
+      });
+    }
+
+    // Hash the password and create a new user
+    bcrypt.hash(password, 10, async (err, hash) => {
+      await User.create({
+        name: name,
+        email: email,
+        password: hash,
+      });
+    });
+
+    // Respond with success status
+    res.status(200).json({
+      success: true,
+      message: "Login Successful!",
+    });
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };
 
 /*
-  This function handles user login requests.
+  Express route handler: Handles user login requests.
   - Extracts user email and password from the request body.
   - Finds the user in the database based on the provided email.
   - Compares the provided password with the hashed password stored in the database.
@@ -78,39 +90,50 @@ exports.postUserSignUp = async (req, res, next) => {
 */
 exports.postUserLogin = async (req, res, next) => {
   try {
-    console.log(req.body);
     const email = req.body.emailValue;
     const password = req.body.passwordValue;
 
-    await User.findOne({ where: { email: email } }).then((user) => {
-      if (user) {
-        bcrypt.compare(password, user.password, (err, result) => {
-          if (err) {
-            return res
-              .status(500)
-              .json({ success: false, message: "Something went Wrong!" });
-          }
-          if (result == true) {
-            return res.status(200).json({
-              success: true,
-              message: "Login Successful!",
-              token: generateAccessToken(user.id, user.email),
-            });
-          } else {
-            return res.status(401).json({
-              success: false,
-              message: "Password Incorrect!",
-            });
-          }
+    // Find the user in the database
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      // User not found
+      return res.status(404).json({
+        success: false,
+        message: "User doesn't exist!",
+      });
+    }
+
+    // Compare passwords
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        // Internal server error
+        return res.status(500).json({
+          success: false,
+          message: "Something went wrong!",
+        });
+      }
+
+      if (result) {
+        // Passwords match, generate access token
+        return res.status(200).json({
+          success: true,
+          message: "Login Successful!",
+          token: generateAccessToken(user.id, user.email),
         });
       } else {
-        return res.status(404).json({
+        // Incorrect password
+        return res.status(401).json({
           success: false,
-          message: "User doesn't Exists!",
+          message: "Password Incorrect!",
         });
       }
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };

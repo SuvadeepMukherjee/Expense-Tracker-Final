50f5b1287b67c24f5b7fb5b9bdcfe8a795e92926
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-08 14:37:53 +0530

    Added logout feature on homepage

---
+++ public/js/homePage.js
@@ -0,0 +1,17 @@
+const categoryItems = document.querySelectorAll(".dropdown-item");
+const categoryInput = document.querySelector("#categoryInput");
+const categoryBtn = document.querySelector("#categoryBtn");
+const form = document.getElementById("form1");
+const addExpenseBtn = document.getElementById("submitBtn");
+const table = document.getElementById("tbodyId");
+const buyPremiumBtn = document.getElementById("buyPremiumBtn");
+const reportsLink = document.getElementById("reportsLink");
+const leaderboardLink = document.getElementById("leaderboardLink");
+const logoutBtn = document.getElementById("logoutBtn");
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "/user/login";
+}
+
+logoutBtn.addEventListener("click", logout);

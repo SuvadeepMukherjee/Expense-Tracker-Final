fb8c1f69bccced5c446ff331657772d55a9836c4
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-09 07:53:30 +0530

    Reports page working

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -8,3 +8,35 @@ const rootDir = require("../util/path");
 exports.getReportsPage = (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "reports.html"));
 };
+
+exports.dailyReports = async (req, res, next) => {
+  try {
+    const date = req.body.date;
+    const expenses = await Expense.findAll({
+      where: { date: date, userId: req.user.id },
+    });
+    return res.send(expenses);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.monthlyReports = async (req, res, next) => {
+  try {
+    const month = req.body.month;
+
+    const expenses = await Expense.findAll({
+      where: {
+        date: {
+          [Op.like]: `%-${month}-%`,
+        },
+        userId: req.user.id,
+      },
+      raw: true,
+    });
+
+    return res.send(expenses);
+  } catch (error) {
+    console.log(error);
+  }
+};

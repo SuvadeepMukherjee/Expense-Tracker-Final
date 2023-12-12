7314f1fcf2bcdeca98cc754462cd636c1ef4ad0c
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-12 13:08:01 +0530

    Added some comments in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,17 @@ const User = require("../models/userModel");
 This middleware (authenticate) checks for a valid JWT in the "Authorization" header. 
 If the token is valid, it retrieves the corresponding user from the database and attaches it to the request
  (req.user). If any error occurs, it logs the error and sends a 401 Unauthorized response with { success: false }.
+when we generate the token we are encoding in this format 
+{
+  "userId": 9,
+  "email": "helal@zohomail.in",
+  "iat": 1702366097
+}
 */
 const authenticate = (req, res, next) => {
   try {
     const token = req.header("Authorization");
     const user = jwt.verify(token, process.env.TOKEN);
-    console.log(user);
     User.findByPk(user.userId).then((user) => {
       req.user = user;
       next();
